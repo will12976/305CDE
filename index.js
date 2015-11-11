25537ea88c61118dbc3c6fd9d95ab17eb47eec3f
will12976 <fletchew@uni.coventry.ac.uk>
2015-11-11 15:04:29 +0000

    bit by bit

--- index.js
+++ index.js
@@ -12,11 +12,36 @@ server.use(restify.authorizationParser())
 var maps = require('./maps.js')
 var accounts = require('./accounts.js')
 
+server.get('/', function(req, res, next) {
+  res.redirect('/maps', next)
+})
+
 server.get('/maps', function (req, res, next) {
   res.send(req.params);
   return next();
 });
- 
-server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
-});
+
+ server.post('/accounts', function(req, res) {
+  console.log('POST /accounts')
+  const auth = req.authorization
+  const body = req.body
+  console.log(body)
+  const host = req.headers.host
+  console.log(typeof req.files)
+  accounts.add(host, auth, body, req.files, function(data) {
+    console.log('DATA RETURNED')
+    console.log(data)
+    res.setHeader('content-type', 'application/json');
+    res.send(data.code, data.response);
+    res.end();
+  })
+})
+
+var port = process.env.PORT || 8080;
+server.listen(port, function (err) {
+  if (err) {
+      console.error(err);
+  } else {
+    console.log('App is ready at : ' + port);
+  }
+})
