dc3b63120f0da5103b93fcd092d5f45c481b188a
will12976 <fletchew@uni.coventry.ac.uk>
2015-11-12 23:55:26 +0000

    Getting there with the third-party api!

--- index.js
+++ index.js
@@ -9,33 +9,40 @@ server.use(restify.queryParser())
 server.use(restify.bodyParser())
 server.use(restify.authorizationParser())
 
-var maps = require('./maps.js')
+var maps = require('./maps.js') //Links to the maps.js module
 var accounts = require('./accounts.js')
 
-server.get('/', function(req, res, next) {
+server.get('/', function(req, res, next) { //Incase the user doesn't retrieve via /maps, it will redirect
   res.redirect('/maps', next)
 })
 
-server.get('/maps', function (req, res, next) {
-  res.send(req.params);
-  return next();
-});
-
- server.post('/accounts', function(req, res) {
-  console.log('POST /accounts')
-  const auth = req.authorization
-  const body = req.body
-  console.log(body)
-  const host = req.headers.host
-  console.log(typeof req.files)
-  accounts.add(host, auth, body, req.files, function(data) {
-    console.log('DATA RETURNED')
+server.get('/maps', function (req, res){ //Start of getting a request back from GET, and whether it is sucessful or not
+  console.log('GET /maps')
+  const searchPlace = req.query.q
+  console.log('address='+searchPlace)
+  maps.search(searchPlace, function(data){
     console.log(data)
-    res.setHeader('content-type', 'application/json');
+    res.setHeader('content-type', 'application/json') // The results that come back, will return as JSON text
     res.send(data.code, data.response);
     res.end();
   })
-})
+});
+
+//server.post('/accounts', function(req, res) {
+//  console.log('POST /accounts')
+//  const auth = req.authorization
+//  const body = req.body
+//  console.log(body)
+//  const host = req.headers.host
+//  console.log(typeof req.files)
+//  accounts.add(host, auth, body, req.files, function(data) {
+//    console.log('DATA RETURNED')
+//    console.log(data)
+//    res.setHeader('content-type', 'application/json');
+//    res.send(data.code, data.response);
+//    res.end();
+//  })
+//})
 
 var port = process.env.PORT || 8080;
 server.listen(port, function (err) {
--- maps.js
+++ maps.js
@@ -4,12 +4,26 @@ var request = require('request')
 //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false
 //This link above avoids having to use a API Code
 
-exports.search = function(address, callback) {
+exports.search = function(query, callback) {
     
     console.log("Searching, won't take long!")
-    if(typeof address !== 'string' || address.length === 0){
+    if(typeof query !== 'string' || query.length === 0){
         callback({code: 400, response:{status:'error', message:'missing query - enter correct parameters'}})
     }
     const url = 'https://maps.googleapis.com/maps/api/geocode/json'
-    const query_string = {a: address, fields: results('address_components'('long_name'))
+    const query_string = {q: query, fields: 'results(address_components(long_name))'}
+    request.get({url:url, qs: query_string, function(err, res, body){
+        if(err){
+            callback({code:500, response:{status:'error', message: "Sorry, I am not receiving data at the moment"}})
+        }
+        console.log(typeof body)
+        const json = JSON.parse(body)
+        const mapObject = json.items
+        const maps = items.map(function(element){
+            return {address_components: element.address_components, long_name: element.address_components.long_name}
+        })
+        console.log(maps)
+        callback({code:200, response:{status:'success', message: 'region found', data:maps}})
+    }
+    })
 }
