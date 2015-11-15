4a864c3e1eb96cc8fb1f1a7ae141d84b3e1fe81f
will12976 <fletchew@uni.coventry.ac.uk>
2015-11-15 02:36:31 +0000

    Meh

--- index.js
+++ index.js
@@ -13,19 +13,18 @@ var films = require('./films.js') //Links to the maps.js module
 var accounts = require('./accounts.js')
 var mongo = require('./mongo.js')
 
+
 const stdin = process.openStdin()
 
 stdin.on('data', function(chunk) {
   console.log(typeof chunk)
   var text = chunk.toString().trim()
   console.log(typeof text)
-
   if (text.indexOf('list') === 0) {
     mongo.getAll( function(data){
         console.log(data)
     })
   }
-  
 })
 
 server.get('/', function(req, res, next) { //Incase the user doesn't retrieve via /maps, it will redirect
@@ -38,9 +37,13 @@ server.get('/films', function (req, res){ //Start of getting a request back from
   console.log('films='+searchPlace)
   films.search(searchPlace, function(data){
     console.log(data);
+    mongo.addList(function(data){
+      console.log('This is working!')
+    })
     res.setHeader('content-type', 'application/json') // The results that come back, will return as JSON text
-    res.send(data.code, data.response);
+    res.send(data.code, data.response, data.film);
     res.end();
+
   })
 });
 
