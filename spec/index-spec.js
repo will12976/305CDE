5047518fce685dc097fc2ff65c954ad9677f86b1
will12976 <fletchew@uni.coventry.ac.uk>
2015-11-22 00:10:42 +0000

    Starting with testing

---
+++ spec/index-spec.js
@@ -0,0 +1,9 @@
+//Testing here is to check if you receive a status code of 200.
+//Then testing if you receive the data.
+
+//Check out this example
+
+//https://www.distelli.com/docs/tutorials/test-your-nodejs-with-jasmine
+
+
+//The idea with testing is simple, show that you are testing if something works, and also show understanding by testing something that you know will fail. 
--- spec/mongo-spec.js
+++ spec/mongo-spec.js
@@ -1,9 +1,22 @@
-var test = require('../mongo.js');
+var mongo = require('../mongo.js');
 
-describe('Getting and storing data', function(){
+describe("Checking the callbacks to see if it returns correctly. It", function() {
     
-    it('should clear the database'), function(){
-        test.clear();
-        expect(test.toBe(1))
-    }
-})
+  it("should log 'lists removed'", function() {
+
+    mongo.clear(function (result){
+      
+      expect(result).toEqual('list') //This doesn't seem to work. Console log returns what I want.
+      console.log(result); //Even equalling only 'list', it still passes.
+      
+    });
+  });
+  
+  //it("should log an error instead", function() {
+  //  mongo.clear(function (result){
+  //    expect(result).not.toBe('lists removed')
+  //    console.log(result)
+  //  })
+  //})
+  
+});
